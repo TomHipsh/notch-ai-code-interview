e1194226ee3ab056236cceb80324ac724349976c
Tom Hipsh <33371239+TomHipsh@users.noreply.github.com>
2026-05-24 18:28:01 +0300

    fix port
    fix port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/healthCheck': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const devServerPort = Number(env.VITE_DEV_SERVER_PORT ?? 5000);
+
+  return {
+    plugins: [react()],
+    server: {
+      port: devServerPort,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/healthCheck': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 })
